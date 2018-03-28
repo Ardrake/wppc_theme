6b9893872731b6663e3c22146b14960939fb7774
André Cooke <andrecooke@hotmail.com>
2018-03-28 14:04:53 -0400

    bootstrap theme

--- functions.php
+++ functions.php
@@ -1,12 +1,28 @@
 <?php 
 
+/*
+ =====================================================================
+  Include Scripts 
+ =====================================================================
+*/
 function wppc_script_enqueue(){
-	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/wppc-theme.css', array(), 'V0.1 Alpha', 'all');
-	wp_enqueue_script('customjs',   get_template_directory_uri() . '/js/wppc-theme.js',   array(), 'V0.1 Alpha',  true);
+        //css
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), 'V4.0.0', 'all');
+        wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/wppc-theme.css', array(), 'V0.1 Alpha', 'all');
+        //JS
+	wp_enqueue_script('jquery');
+        wp_enqueue_script('bootstrapjs',   get_template_directory_uri() . '/js/bootstrap.min.js',   array(), 'V4.0.0',  true);
+        wp_enqueue_script('customjs',   get_template_directory_uri() . '/js/wppc-theme.js',   array(), 'V0.1 Alpha',  true);
+        
 }
 
 add_action('wp_enqueue_scripts', 'wppc_script_enqueue');
 
+/*
+=======================================================================
+ Activate menus
+=======================================================================
+ */
 function wppc_theme_setup() {
     
     add_theme_support('menus');
@@ -17,4 +33,11 @@ function wppc_theme_setup() {
 }
 
 add_action('init', 'wppc_theme_setup');
+
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
+
+add_theme_support('post-formats', array('aside', 'image', 'video'));
+
 
--- index.php
+++ index.php
@@ -4,11 +4,7 @@
     if (have_posts()):
         while (have_posts()): the_post(); ?>
 
-            <h3><?php the_title();  ?> </h3>
-            <small>posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a');?>, in <?php the_category();?></small>
-            <p><?php the_content();?> </p>
-            
-            <hr>
+            <?php get_template_part('content', get_post_format());  ?>
             
         <?php endwhile;
     endif;
